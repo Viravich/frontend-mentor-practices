388b5f8df77142e64eaaec8ecd52781343201893
viarvich <viarvich.phi@kkumail.com>
2022-05-02 22:58:26 +0700

    build stats preview card by react

---
+++ stats-preview-card-component-main/stats-preview-card/src/App.js
@@ -0,0 +1,13 @@
+import CardComponent from "./components/Cards";
+import Apps from "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <main className="App">
+      <CardComponent />
+    </main>
+  );
+}
+
+export default App;
